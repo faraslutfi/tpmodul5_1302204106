781b931e17850f3504b57ba5ebde3dbcaa40d9c3
eliaangga <127936502+faraslutfi@users.noreply.github.com>
2025-03-16 14:55:01 +0700

    Menambahkan class DataGeneric dengan method PrintData

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿using tpmodul5_1302204106;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("1302204106");  // memanggil method dengan input NIM
+
+        DataGeneric<string> data = new DataGeneric<string>("1302204106");
+        data.PrintData();  // Memanggil PrintData() untuk mencetak data yang tersimpan
+
+        Console.ReadLine(); // supaya console ga ketutup
+    }
+}
+
